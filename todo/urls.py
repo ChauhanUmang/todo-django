f3ea768c1eace5035aeafe731aec0e6fcd483820
Umang Chauhan <umang4191@outlook.com>
2023-07-22 21:41:19 +0200

    Mark as done feature completed

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,11 @@ from todo import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name="addTask"),
+    # in the url, whatever name we have given after int:<variable name>
+    # should be same as the name in the function in the view
+    # here, we have named it task_id, so we should have a task_id parameter
+    # in the function
+    # if named it pk, like we usually do, we should have a parameter named pk
+    # in the function
+    path('mark_as_done/<int:task_id>/', views.mark_as_done, name="mark_as_done"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from todo.models import Task
 
@@ -8,3 +8,10 @@ def addTask(request):
     added_task = request.POST['added_task']
     Task.objects.create(task=added_task)
     return redirect('home')
+
+
+def mark_as_done(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.is_completed = True
+    task.save()
+    return redirect("home")
